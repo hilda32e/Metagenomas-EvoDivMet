ddd2d4df07b06e36790c88d510f5896be3baba11
EdderDaniel <ddr.dnl@gmail.com>
2019-02-26 15:36:43 -0600

    README update

--- Metagenome_Analysis/Taxonomy/Taxonomic_analysis.sh
+++ Metagenome_Analysis/Taxonomy/Taxonomic_analysis.sh
@@ -17,24 +17,24 @@ ASSEMBLY_dir=$4
 if [ -z ${ASSEMBLY_dir+x} ]; then ASSEMBLY_dir=$4; else ASSEMBLY_dir=$root/ASSEMBLIES; fi 
 
 #makes output directories
-mkdir ANALYSIS 
-mkdir ANALYSIS_LOGS
-mkdir ANALYSIS_LOGS/OUTPUTS
-mkdir ANALYSIS_LOGS/SCRIPTS
+mkdir TAXONOMY 
+mkdir TAXONOMY_LOGS
+mkdir TAXONOMY_LOGS/OUTPUTS
+mkdir TAXONOMY_LOGS/SCRIPTS
 
 
 #This script will run the kraken taxonomic assignment for the read files and for every assembly. 
-#The kraken and kraken.report files are located in ANALYSIS/KRAKEN
+#The kraken and kraken.report files are located in TAXONOMY/KRAKEN
 #This script also run the bracken taxonomic correction for the kraken output. 
-#The bracken file is located in ANALYSIS/BRACKEN and the bracken.report is outputed in ANALYSIS/KRAKEN
-cat  > anlKRAKEN_BRACKEN.sh <<EOF
+#The bracken file is located in TAXONOMY/BRACKEN and the bracken.report is outputed in TAXONOMY/KRAKEN
+cat  > txnmKRAKEN_BRACKEN.sh <<EOF
 
 #PBS -N ${suffix}_KRAKEN&BRACKEN
 #PBS -q ensam
 #PBS -l nodes=1:ppn=12,mem=50g,vmem=50g
 #PBS -q ensam
-#PBS -e ${root}/ANALYSIS_LOGS/OUTPUTS/${suffix}_KRAKEN_BRACKEN.error
-#PBS -o ${root}/ANALYSIS_LOGS/OUTPUTS/${suffix}_KRAKEN_BRACKEN.output
+#PBS -e ${root}/TAXONOMY_LOGS/OUTPUTS/${suffix}_KRAKEN_BRACKEN.error
+#PBS -o ${root}/TAXONOMY_LOGS/OUTPUTS/${suffix}_KRAKEN_BRACKEN.output
 #PBS -V
 
 module load kraken/2.0.7
@@ -42,18 +42,18 @@ module load Braken/2.0
 
 cd $root
 
-mkdir ANALYSIS/KRAKEN
-mkdir ANALYSIS/BRACKEN
+mkdir TAXONOMY/KRAKEN
+mkdir TAXONOMY/BRACKEN
 
-kraken2 --db kraken-db --threads 12 --paired --fastq-input $FILE1 $FILE2 --output ANALYSIS/KRAKEN/${suffix}_kraken.kraken --report ANALYSIS/KRAKEN/${suffix}_kraken.report
-for i in $ASSEMBLY_dir/*fasta; do OUTPUTNAME=$sign(echo ${sign}i | sed "s/.*\///" | cut -d'.' -f1); kraken2 --db kraken-db --fasta-input ${sign}i --threads 12 --output ANALYSIS/KRAKEN/${suffix}_"${sign}OUTPUTNAME"_kraken.kraken --report ANALYSIS/KRAKEN/"${sign}OUTPUTNAME"_kraken.report; done 
-for a in ANALYSIS/KRAKEN/*.report; do OUTPUTNAMEBRACKEN=${sign}(echo ${sign}a | sed "s/.*\///" | cut -d'.' -f1); bracken -d kraken-db -i ${sign}a -o ANALYSIS/BRACKEN/"${sign}OUTPUTNAMEBRACKEN".bracken; done
+kraken2 --db kraken-db --threads 12 --paired --fastq-input $FILE1 $FILE2 --output TAXONOMY/KRAKEN/${suffix}_kraken.kraken --report TAXONOMY/KRAKEN/${suffix}_kraken.report
+for i in $ASSEMBLY_dir/*fasta; do OUTPUTNAME=$sign(echo ${sign}i | sed "s/.*\///" | cut -d'.' -f1); kraken2 --db kraken-db --fasta-input ${sign}i --threads 12 --output TAXONOMY/KRAKEN/${suffix}_"${sign}OUTPUTNAME"_kraken.kraken --report TAXONOMY/KRAKEN/"${sign}OUTPUTNAME"_kraken.report; done 
+for a in TAXONOMY/KRAKEN/*.report; do OUTPUTNAMEBRACKEN=${sign}(echo ${sign}a | sed "s/.*\///" | cut -d'.' -f1); bracken -d kraken-db -i ${sign}a -o TAXONOMY/BRACKEN/"${sign}OUTPUTNAMEBRACKEN".bracken; done
 EOF
 
-#Submits the analysis scripts to the cluster
-for i in anl*; do
+#Submits the TAXONOMY scripts to the cluster
+for i in txnm*; do
         qsub $i
 done
 
-#moves the analysis scripts to the corresponding sub-folder
-mv anl* ANALYSIS_LOGS/SCRIPTS
+#moves the TAXONOMY scripts to the corresponding sub-folder
+mv txnm* TAXONOMY_LOGS/SCRIPTS
